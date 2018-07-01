715d4c2074e07048fcd9c9e956678bc8fb8c1e54
David Hoffman <david@mmxca.com>
2018-07-01 19:39:19 +0000

    Initial Commit

---
+++ src/HelperFunctions.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HelperFunctions;
+
+/**
+ * HelperFunctions\HelperFunctions.
+ *
+ * Provides repeatable use functions that should have been part of PHP but weren't
+ *
+ * @author  David Hoffman
+ *
+ * @version 0.0.1
+ */
+class HelperFunctions
+{
+    /**
+     * Turns a multiword string into a single word.
+     *
+     * @param string $string The String you want to slugify
+     * @param string $joiner (optional) The string to concatinate with, default is -
+     */
+    public static function slugify($string, $joiner = '-')
+    {
+        return strtolower(
+            str_replace(' ', $joiner, str_replace('  ', ' ', str_replace(',', '', str_replace('.', '', str_replace('&', '', ucwords(strtolower(str_replace('_', ' ', $string))))))))
+        );
+    }
+}
